732d9b260d0ebc7fd8a48aecffcf7610c433ce99
Duane McFarlane <duanemcfarlane@yahoo.com>
2018-06-26 22:34:11 -0400

    updated JS scroll feature

--- main.js
+++ main.js
@@ -31,3 +31,55 @@ $("#scroll").click(function() {
     $('html, body').animate({scrollTop:$(document).height()}, 'slow');
     return false;
   });
+
+  $(document).ready(function () {
+    $('li.bmr_click').click(function() {
+    $('html, body').animate({
+      scrollTop: $("#bmr_head_1").offset().top
+    }, 'slow')
+  }), 
+    $('li.photo_click').click(function (){
+      $('html, body').animate({
+        scrollTop: $("#photo_head_1").offset().top
+      }, 'slow')
+    }),
+    $('li.sign_up_click').click(function (){
+      $('html, body').animate({
+        scrollTop: $("h1.sign_up").offset().top
+      }, 'slow')
+    }),
+    $('li.about_click').click(function (){
+      $('html, body').animate({
+        scrollTop: $("div.about_us").offset().top
+      }, 'slow')
+    })
+  });
+
+  $('li.bmr_click').hover(function() {
+    $(this).toggleClass('active')
+    $('li.about_click').toggleClass('active');
+  },
+  function(){
+    $(this).toggleClass('active')
+    $('li.about_click').toggleClass('active');
+  });
+
+  $('li.photo_click').hover(function() {
+    $(this).toggleClass('active')
+    $('#photo_head_1').toggleClass('active')
+    $('li.about_click').toggleClass('active');
+  },
+  function(){
+    $(this).toggleClass('active')
+    $('li.about_click').toggleClass('active');
+  });
+
+  $('li.sign_up_click').hover(function() {
+    $(this).toggleClass('active')
+    $('#photo_head_1').toggleClass('active')
+    $('li.about_click').toggleClass('active');
+  },
+  function(){
+    $(this).toggleClass('active')
+    $('li.about_click').toggleClass('active');
+  });
